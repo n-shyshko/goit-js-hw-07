4c47a14f013f3a25d4921c940e6104e95091de2b
nik <nshishko@gmail.com>
2024-07-06 17:51:22 +0300

    update hw7

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", colorHandler);
+
+function colorHandler() { 
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  textColor.textContent = randomColor;
+}
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = { 
+  number: document.querySelector("input[type=number]"),
+  createBtn:document.querySelector("button[data-create]"),
+  destroyBtn:document.querySelector("button[data-destroy]"),
+  boxesContainer:document.querySelector("#boxes")
+}
+
+refs.createBtn.addEventListener("click", handleSqrCreation);
+refs.destroyBtn.addEventListener("click", andleSqrDestroy);
+
+function handleSqrCreation() {
+  let number = parseInt(refs.number.value);
+  const minValue = parseInt(refs.number.attributes.min.value);
+  const maxValue = parseInt(refs.number.attributes.max.value);
+
+  if (number >= minValue && number <= maxValue) { 
+    createBoxes(number);
+  }
+  refs.number.value = null;
+}
+
+function andleSqrDestroy() {
+  destroyBoxes()
+ }
+
+function createBoxes(amount) {
+  const boxes = [];
+  let initialWidth = 30;
+  let initialHeight = 30;
+  for (let index = 0; index < amount; index += 1) {
+    const boxRandomColor = getRandomHexColor();
+    let box = `<div class="js-box" style="background-color:${boxRandomColor};width: ${initialWidth}px; height: ${initialHeight}px"></div>`;
+    boxes.push(box);
+    initialWidth += 10;
+    initialHeight += 10;
+  }
+   refs.boxesContainer.innerHTML = boxes.join(""); 
+  }
+
+function destroyBoxes() {
+  refs.boxesContainer.innerHTML = "";
+}
